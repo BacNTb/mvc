acb9e91b821a39415e443a131e4446b8e5194688
Bacnt <thebac9910@gmail.com>
2020-09-10 15:50:36 +0700

    Commit file MVC

---
+++ dispatcher.php
@@ -0,0 +1,35 @@
+<?php
+    namespace MVC\Dispatcher;
+
+    use MVC\Request\Request;
+
+    use MVC\Router\Router;
+
+    // use MVC\Controller\tasksController;
+
+    class Dispatcher
+    {
+        private $request;
+
+        public function dispatch()
+        {
+            $this->request = new Request();
+            
+            Router::parse($this->request->url, $this->request);
+            
+            $controller = $this->loadController();
+
+            call_user_func_array([$controller, $this->request->action], $this->request->params);
+        }
+
+        public function loadController()
+        {
+            $name = $this->request->controller . "Controller";
+            $file = ROOT . 'Controllers/' . $name . '.php';
+            require($file);
+            $controller = new $name();
+            return $controller;
+        }
+
+    }
+?>
